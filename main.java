befc5a7d3e086c5a77c1593fbc4eabed6b2b99ea
Chuck476 <cdierbach@towson.edu>
2024-01-26 19:19:01 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+// Practicum 11 (Largest of Three Program)
+// (TO COMPLETE)
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // var declarations
+        double num1, num2, num3, largest;
+
+        // prompt user for three numbers
+       
+
+        // determine largest and assign to variable largest
+        
+
+        // display results
+        System.out.println("The largest of " + num1 + ", " + num2 +
+                           ", " + num3 + " is " + largest);
+    }
+}
